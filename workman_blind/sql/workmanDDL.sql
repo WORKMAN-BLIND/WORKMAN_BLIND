14f9f5d561b968d25e101f9710b0b1767f684214
JANGMOONHEE <reolove7029@gmail.com>
2021-01-11 23:26:42 +0900

    ddl,dml last update

--- workman_blind/sql/workmanDDL.sql
+++ workman_blind/sql/workmanDDL.sql
@@ -13,50 +13,47 @@ DROP TABLE company cascade constraint;
 
 CREATE TABLE company (
    company_name      VARCHAR2(50)    PRIMARY KEY,
-   company_pw        VARCHAR2(20)    NOT NULL,   
+   company_pw        VARCHAR2(50)    NOT NULL,   
+   category          VARCHAR2(50)    NOT NULL,
    company_loc       VARCHAR2(200)    NOT NULL,
-   company_num       VARCHAR2(30)    NOT NULL,
-   category          VARCHAR2(40)    NOT NULL   
+   company_num       VARCHAR2(50)    NOT NULL
 );
 
 
 CREATE TABLE member (
-       user_id    VARCHAR2(20)  PRIMARY KEY,
+       user_id    VARCHAR2(50)  PRIMARY KEY,
+       company_name 	VARCHAR2(50) NOT NULL,
        user_pw    VARCHAR2(20) NOT NULL,
        user_name    VARCHAR2(20) NOT NULL,
-       user_num    VARCHAR2(50) NOT NULL,
-       company_name          VARCHAR2(50) NOT NULL
+       user_num    VARCHAR2(50) NOT NULL
 );
 
 
 CREATE TABLE parttimelist (
-       text_list             NUMBER(10) PRIMARY KEY,
+       list_num             NUMBER(10) PRIMARY KEY,
        company_name          VARCHAR2(50) NOT NULL,
-       user_id              VARCHAR2(50) NOT NULL,
-       review_num          NUMBER(10) NOT NULL,
-       review_score          NUMBER(10) NOT NULL,
-       recruit_status        VARCHAR2(50) NOT NULL,
-       regist_date          VARCHAR2(50) NOT NULL,
-       emp_period          VARCHAR2(50) NOT NULL
+       work_time              VARCHAR2(50) NOT NULL,
+       hourly_wage       VARCHAR2(50) NOT NULL,
+       work_period          VARCHAR2(50) NOT NULL,
+       objective         VARCHAR2(50) NOT NULL
 );
 
 
 CREATE TABLE parttimeeval (
       text_eval            NUMBER(10) PRIMARY KEY,
-       text_list            NUMBER(10) NOT NULL,
        user_id            VARCHAR2(50) NOT NULL,
        company_name         VARCHAR2(50) NOT NULL,
-       pros_cons           VARCHAR2(50) NOT NULL,
-       wage               Number(10) NOT NULL,
-       environment          VARCHAR2(50) NOT NULL,
-       incline              VARCHAR2(50) NOT NULL,
-       work_dif            VARCHAR2(50) NOT NULL,
-       experience         VARCHAR2(4000) NOT NULL
-);
+       pros_cons           VARCHAR2(100) NOT NULL,
+       wage_delay             VARCHAR2(100) NOT NULL,
+       environment          VARCHAR2(100) NOT NULL,
+       incline              VARCHAR2(100) NOT NULL,
+       opinion         VARCHAR2(200) NOT NULL
+       );
 
 
 
 
 ALTER TABLE member  ADD FOREIGN KEY (company_name) REFERENCES company (company_name);
-ALTER TABLE parttimelist ADD FOREIGN KEY (user_id)  REFERENCES member (user_id);
-ALTER TABLE parttimeeval ADD FOREIGN KEY (text_list) REFERENCES parttimelist (text_list);
+ALTER TABLE parttimelist ADD FOREIGN KEY (company_name)  REFERENCES company (company_name);
+ALTER TABLE parttimeeval ADD FOREIGN KEY (company_name)  REFERENCES company (company_name);
+ALTER TABLE parttimeeval ADD FOREIGN KEY (user_id) REFERENCES member (user_id);
